7dcc64a98348dd75c9e0607717ddb41db7bfe4f6
Rodri1996 <69223756+Rodri1996@users.noreply.github.com>
2021-09-09 15:47:00 -0300

    Criterio Social ahora devuelve un Set de Usuarios
    Se cambia el Array de Usuarios por un Set de Usuarios.Para que no haya usuarios repetidos

--- Typescript/src/CriterioDeEdicion/criterioDeEdicion.ts
+++ Typescript/src/CriterioDeEdicion/criterioDeEdicion.ts
@@ -23,11 +23,11 @@ export class Amistoso {
 
 export class Social {
     rutinaPuedeSerEditadaPor(unUsuario:Usuario , unaRutina:Rutina):boolean{
-        return this.seguidoresDeLaRutina(unaRutina).includes(unUsuario);
+        return this.seguidoresDeLaRutina(unaRutina).has(unUsuario);
     }
 
-    seguidoresDeLaRutina(rutina:Rutina):Usuario[]{
-        return rutina.seguidores;
+    seguidoresDeLaRutina(rutina:Rutina):Set<Usuario>{
+        return new Set(rutina.seguidores);
     }
 }
 
