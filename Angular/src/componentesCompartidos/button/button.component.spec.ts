034c9f0f04e81c8009b08f042b42ee03ab9dce0c
stephaniepazos89 <stephaniepazos89@gmail.com>
2021-09-22 12:57:23 -0300

    agregue routinlink al boton ingresar y arme el login

--- Angular/src/componentesCompartidos/button/button.component.spec.ts
+++ Angular/src/componentesCompartidos/button/button.component.spec.ts
@@ -1,28 +1,28 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
+import { DebugElement } from '@angular/core'
 
-import { ButtonComponent } from './button.component';
+import { ButtonComponent } from './button.component'
 
 describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+  let component: ButtonComponent
+  let fixture: ComponentFixture<ButtonComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ButtonComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(ButtonComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
