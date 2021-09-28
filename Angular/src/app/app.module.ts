507f7ece95b75b2ec432aa6bff061e252f82b115
stephaniepazos89 <stephaniepazos89@gmail.com>
2021-09-28 01:11:05 -0300

    agregamos el ejercicio component al ngmodel y un input a ejercicio

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,19 +1,22 @@
 
+
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing/app-routing.module'
 import { AppComponent } from './app.component'
 
-import { MarcadorComponent } from './../componentesCompartidos/marcador/marcador.component'
+import { EjercicioComponent } from 'src/componentesPadres/ejercicio/ejercicio.component'
 import { UsuarioComponent } from 'src/componentesPadres/usuario/usuario.component'
-import { ButtonComponent } from 'src/componentesCompartidos/button/button.component'
+import { RutinaComponent } from 'src/componentesPadres/rutina/rutina.component'
 import { LoginComponent } from 'src/componentesPadres/login/login.component'
+
+import { MarcadorComponent } from './../componentesCompartidos/marcador/marcador.component'
+import { ButtonComponent } from 'src/componentesCompartidos/button/button.component'
 import { FooterComponent } from '../componentesCompartidos/footer/footer.component'
 import { NavbarComponent } from 'src/componentesCompartidos/navbar/navbar.component'
 import { MenuDesplegableComponent } from 'src/componentesCompartidos/menuDesplegable/menuDesplegable.component'
 import { CardComponent } from 'src/componentesCompartidos/card/card.component'
-import { RutinaComponent } from 'src/componentesPadres/rutina/rutina.component'
 import { InputComponent } from 'src/componentesCompartidos/input/input.component'
 
 
@@ -30,6 +33,7 @@ import { InputComponent } from 'src/componentesCompartidos/input/input.component
     MarcadorComponent,
     RutinaComponent,
     InputComponent,
+    EjercicioComponent
   ],
   imports: [
     BrowserModule,
