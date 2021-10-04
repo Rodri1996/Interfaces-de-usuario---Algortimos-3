40fb6242da39c3866e8c357591ad2f893c6de467
stephaniepazos89 <stephaniepazos89@gmail.com>
2021-10-04 19:44:14 -0300

    sacando header en login

--- Angular/src/componentesCompartidos/navbar/navbar.component.ts
+++ Angular/src/componentesCompartidos/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common'
+import { Component} from '@angular/core'
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
 
-  constructor() { }
+export class NavbarComponent {
 
-  ngOnInit() {
-  }
+  noEsLogin = () => this.location.path() !== '/login'
 
+  constructor(private location:Location){}
+
+  
 }
+
