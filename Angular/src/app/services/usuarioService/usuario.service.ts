b110ce398ef97935ff6e1ffe423f05719aa4315c
stephaniepazos89 <stephaniepazos89@gmail.com>
2021-10-20 00:12:15 -0300

    cambios en usuario service y login

--- Angular/src/app/services/usuarioService/usuario.service.ts
+++ Angular/src/app/services/usuarioService/usuario.service.ts
@@ -1,21 +1,15 @@
-import { Injectable } from "@angular/core"
-import { Usuario } from "src/domain/Usuario/usuario"
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Usuario } from 'src/domain/Usuario/usuario'
 
- @Injectable({
-     providedIn: 'root'
- })
- export class UsuarioService{
+@Injectable({
+    providedIn: 'root'
+})
 
-    private usuarios:Usuario[] =[
-        new Usuario(1,'pepe','123'),
-        new Usuario(2,'pedro','321')
-    ]
+export class UsuarioService {
+    constructor(public http: HttpClient) { }
 
-    loguearUsuario(username:string, password:string):Promise<Usuario>{
-        return new Promise((resolve) => {
-       const usuarioBuscado=this.usuarios.filter((usuario:Usuario) => username === usuario.username && password === usuario.password)[0]
-       if(!usuarioBuscado) throw Error("Usuario y/o contraseña es invalido") 
-       resolve(usuarioBuscado)
-       })
+         loguearUsuario(username: string, password: string) {
+        return this.http.post<Usuario>('localhost:8080' + '/login', {username, password}).toPromise()
     }
- }
+}
